667e91491210dac881487415393a32c2c0be54de
Jacob Kelley <jakie8@gmail.com>
2013-10-17 15:17:33 -0700

    GH Pages Commit

---
+++ assets/misc.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	
+	var api = impress();
+	api.init();
+
+	$(document).on('click', '#header ul a', function(e){
+		e.preventDefault();
+		var $this = $(this);
+		$('#header a.active').removeClass('active');
+		$this.addClass('active');
+		
+		api.goto($this.data('target')+'-1');
+
+	});
+
+	var hashChange = function(){
+		var hash = window.location.hash;
+		hash = hash.replace('#/', '');
+		hash = hash.split('-')[0];
+		
+		if( $('[data-target="'+hash+'"].active').length==0 ){
+			$('#header a.active').removeClass('active');
+			$('[data-target="'+hash+'"]').addClass('active');
+		}
+	};
+
+	hashChange();
+	window.onhashchange = hashChange;
+
+});
